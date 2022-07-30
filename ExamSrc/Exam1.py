096f0d0014f5598813fc619aad35bcd3d1a54e9f
JeongHyeSoo <nailonly@naver.com>
2022-07-30 13:51:33 +0900

    k번째 약수

--- ExamSrc/Exam1.py
+++ ExamSrc/Exam1.py
@@ -1,3 +1,16 @@
-name="My name is HyeSoo"
-print(name)
-print()
+"""
+두 개의 자연수 N과 K가 주어졌을 때, N의 약수들 중 K번째로 작은 수를 출력하는 프로그램을
+작성하시오.
+"""
+import sys
+sys.stdin=open("input.txt","rt")
+n, k=map(int, input().split())
+cnt=0
+for i in range(1,n+1):
+    if n%i==0:
+        cnt+=1
+    if cnt==k:
+        print(i)
+        break
+else:
+    print(-1)
